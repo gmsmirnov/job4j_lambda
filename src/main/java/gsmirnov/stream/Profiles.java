e7f268a436a3302c578130f7eb107a105176d202
gsmirnov <artress@ngs.ru>
2020-04-05 12:51:53 +0700

    Уникальность элементов и сортировка. [#110225] (fix_02)

--- src/main/java/gsmirnov/stream/Profiles.java
+++ src/main/java/gsmirnov/stream/Profiles.java
@@ -8,7 +8,7 @@ import java.util.stream.Collectors;
  * Profiles ti addresses converter.
  *
  * @author Gregory Smirnov (gsmirnov <artress@ngs.ru>)
- * @version 0.1
+ * @version 0.2
  * @since 2020-04-01
  */
 public class Profiles {
@@ -21,7 +21,7 @@ public class Profiles {
      */
     public static List<Address> collect(List<Profile> profiles) {
         return profiles.stream()
-                .map(profile -> new Address(profile.getAddress()))
+                .map(Profile::getAddress)
                 .sorted(Comparator.comparing(Address::getCity))
                 .distinct()
                 .collect(Collectors.toList());
